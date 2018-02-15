d7388e87ac2e868b497290f22621fd560d0cf5cf
Yoatn <vendl@bk.ru>
2018-02-15 09:01:26 +0300

    index + caalog_wear

---
+++ 1.py
@@ -0,0 +1,9 @@
+
+
+
+def get_int(start_message, error_message, end_message):
+    print(start_message)
+
+
+x = get_int('Input int number:', 'Wrong value. Input int number:', 'Thank you.')
+print(x)
