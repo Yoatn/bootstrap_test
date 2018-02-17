9cef21fe01180ff24aacf10a7b4b8fe445ecbc42
Yoatn <vendl@bk.ru>
2018-02-17 21:10:57 +0300

    catalog_wear, index slider

--- 1.py
+++
@@ -1,9 +0,0 @@
-
-
-
-def get_int(start_message, error_message, end_message):
-    print(start_message)
-
-
-x = get_int('Input int number:', 'Wrong value. Input int number:', 'Thank you.')
-print(x)
